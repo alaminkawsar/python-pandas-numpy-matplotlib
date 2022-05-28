c0b58c76e8bbe193328262c86c73d0a7fc99f1ca
alaminkawsar <alaminkawsar8643@gmail.com>
2022-05-28 23:35:02 +0600

    casting

---
+++ Python/casting.py
@@ -0,0 +1,15 @@
+#casting into integer
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#casting into float
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#casting into string
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
