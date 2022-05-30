0c8dca4f8a13079dce5c4e9ff0355beb043cf04c
alaminkawsar <alaminkawsar8643@gmail.com>
2022-05-30 16:28:26 +0600

    cv2_image_read_show

--- OpenCv/cv2_read_imshow.py
+++ OpenCv/cv2_read_imshow.py
@@ -5,6 +5,13 @@ path = '/home/kawsar/Desktop/Deep Learning/python-pandas-numpy-matplotlib/OpenCv
 # Using cv2.imread() method
 image = cv2.imread(path)
 
+#gray scale mode
+#image = cv2.imread(path, 0)
+
+#unchanged
+#image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+
+
 # Displaying the image
 cv2.imshow('image', image)
 cv2.waitKey(0)
