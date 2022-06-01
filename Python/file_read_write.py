14e9957c09bf102ca69961221a30c852b1f2f7b2
alaminkawsar <alaminkawsar8643@gmail.com>
2022-06-01 23:58:28 +0600

    read write file in python

---
+++ Python/file_read_write.py
@@ -0,0 +1,10 @@
+# a file named "geek", will be opened with the reading mode.
+file = open('geek.txt', 'r')
+# This will print every line one by one in the file
+for each in file:
+	print (each)
+
+# Python code to illustrate read() mode
+file = open("file.txt", "r")
+print (file.read())
+
