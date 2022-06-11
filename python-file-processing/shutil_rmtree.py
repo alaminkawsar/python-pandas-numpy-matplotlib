1fbc1966b507f9e9ecf74d922c3b076ca10e965b
alaminkawsar <alaminkawsar8643@gmail.com>
2022-06-11 23:06:27 +0600

    remove folder

---
+++ python-file-processing/shutil_rmtree.py
@@ -0,0 +1,9 @@
+import os
+import random
+import shutil
+
+'''remove directory'''
+
+source_dir = '/home/kawsar/Desktop/Deep Learning/python-pandas-numpy-matplotlib/python-file-processing'
+
+shutil.rmtree(os.path.join(source_dir,'hi'))
