29d5df02d14e7bc0ff33d850cc8c1444b57c7448
alaminkawsar <alaminkawsar8643@gmail.com>
2022-06-11 23:05:35 +0600

    join path

---
+++ python-file-processing/os_path_join.py
@@ -0,0 +1,7 @@
+import os
+import random
+
+print(os.path)
+
+source_dir = '/home/kawsar/Desktop/Deep Learning/python-pandas-numpy-matplotlib/python-file-processing'
+print(os.path.join(source_dir,'New Folder'))
