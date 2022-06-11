840cc8dd942cc90ea5d27649ec97796c0280efbb
alaminkawsar <alaminkawsar8643@gmail.com>
2022-06-12 01:45:24 +0600

    csvfile write

---
+++ python-file-processing/csv_write.py
@@ -0,0 +1,37 @@
+import csv
+import os
+import ossaudiodev
+path = '/home/kawsar/Desktop/Deep Learning/python-pandas-numpy-matplotlib/python-file-processing'
+with open(os.path.join(path,'osinnovators.csv'), 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Name", "Contribution"])
+    writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
+    writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
+    writer.writerow([3, "Guido van Rossum", "Python Programming"])
+
+
+
+'''with writerows'''
+row_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open(os.path.join(path,'writerows.csv'), 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(row_list)
+    
+    
+    
+    
+    
+'''using delimeters'''
+data_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open(os.path.join(path,'delimeters.csv'), 'w', newline='') as file:
+    writer = csv.writer(file, delimiter='|')
+    writer.writerows(data_list)
+    
+    
+
