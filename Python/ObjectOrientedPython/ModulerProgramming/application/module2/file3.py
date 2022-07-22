9c18b06e1efad7dbebfe200de2c470f220b0b03f
alaminkawsar <alaminkawsar8643@gmail.com>
2022-07-22 23:59:25 +0600

    edited

--- Python/ObjectOrientedPython/ModulerProgramming/application/module2/file3.py
+++ Python/ObjectOrientedPython/ModulerProgramming/application/module2/file3.py
@@ -1,4 +1,8 @@
+#Remember one thing.. we need to go the current directory to run this folder.
+
 import sys
+
+#it syas go back first, then find moudule1
 sys.path.insert(0,"..")
  
 from module1.file1 import ArithmeticOperations
