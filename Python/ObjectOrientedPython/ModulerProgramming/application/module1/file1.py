3c61c1415dfb1ca000587381c8abc1904caa8d07
alaminkawsar <alaminkawsar8643@gmail.com>
2022-07-22 23:47:18 +0600

    import all of classes

--- Python/ObjectOrientedPython/ModulerProgramming/application/module1/file1.py
+++ Python/ObjectOrientedPython/ModulerProgramming/application/module1/file1.py
@@ -4,3 +4,14 @@ class ArithmeticOperations:
     def get_sum(self, num1, num2):
         self.sum = num1+num2;
         return self.sum
+    
+
+
+
+class PrintData:
+    def __init__(self) -> None:
+        self.data=""
+    
+    def print_data(self,data):
+        self.data = data
+        return self.data
--- Python/ObjectOrientedPython/ModulerProgramming/application/module1/file2.py
+++ Python/ObjectOrientedPython/ModulerProgramming/application/module1/file2.py
@@ -5,5 +5,16 @@ obj1 = file1.ArithmeticOperations()
 c = obj1.get_sum(3,8)
 print(c)
 
+#another way
 obj2 = ArithmeticOperations()
 print(obj2.get_sum(15,20))
+
+
+
+#import all of classes
+from file1 import*
+obj3 = ArithmeticOperations()
+print(obj3.get_sum(10,10))
+
+obj4 = PrintData()
+print(obj4.print_data("Accessing Another class from file2"))
